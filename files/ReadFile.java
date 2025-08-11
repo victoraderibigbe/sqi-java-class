0faf83c9b00bde4f8f3e685ca3de5cfb79b8f206
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2025-08-11 19:00:32 +0100

    Add file handling lesson

---
+++ files/ReadFile.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ReadFile rf = new ReadFile();
+
+        System.out.print("Enter filename: ");
+        String filename = sc.nextLine();
+        System.out.println();
+
+        System.out.print("Enter filetype (e.g., txt, html): ");
+        String filetype = sc.nextLine();
+        System.out.println();
+
+        rf.readFile(filename, filetype);
+        sc.close();
+    }
+
+    public void readFile(String filename, String filetype) {
+        String fileName = filename + "." + filetype;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ files/WriteFile.java
@@ -0,0 +1,66 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class WriteFile {
+    private File file;
+    private BufferedWriter bw;
+
+    public WriteFile() {
+        file = new File("");
+    }
+
+    public static void main(String[] args) {
+        WriteFile wf = new WriteFile();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the filename (e.g, myfile): ");
+        String filename = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Enter file extension: (e.g, txt, html): ");
+        String fileType = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Enter the text you want to write into " + filename + " : ");
+        String text = scanner.nextLine();
+
+        wf.writeFile(filename, fileType, text);
+        scanner.close();
+    }
+
+    public void createNewFile(String filename) {
+        try {
+            file = new File(filename);
+            if (file.exists()) {
+                System.out.println("File already exists");
+                return;
+            }
+
+            if (this.file.createNewFile()) {
+                System.out.println(file.getName() + " " + "has been created.");
+            } else {
+                System.out.println("File could not be created");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeFile(String filename, String fileType, String text) {
+        String fileName = filename + "." + fileType;
+
+        if (!file.exists()) {
+            this.createNewFile(fileName);
+        }
+
+        try {
+            bw = new BufferedWriter(new FileWriter(fileName));
+            bw.write(text);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
